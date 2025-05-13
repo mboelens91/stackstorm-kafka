1dc1dc2ec55d0f9f7498f29e90b76789d8c051aa
Martijn Boelens <mboelens91@users.noreply.github.com>
2025-05-13 18:06:41 +0200

    Update message_sensor.py

--- sensors/message_sensor.py
+++ sensors/message_sensor.py
@@ -51,8 +51,7 @@ class KafkaMessageSensor(Sensor):
                                        bootstrap_servers=self._hosts,
                                        security_protocol="SSL",
                                        ssl_context=create_default_context(),
-                                       api_version=(2,6),
-                                       value_deserializer=lambda m: m.decode())
+                                       api_version=(2,6))
         self._ensure_topics_existence()
 
     def _ensure_topics_existence(self):
